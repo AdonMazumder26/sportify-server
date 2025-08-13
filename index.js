20f049dc7c1e710caa5a0d74c22eeeb2ce734b6e
adon <ruhulmaani2003@gmail.com>
2025-08-13 20:04:10 +0600

    made an  api for the user related query

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/equipment/email/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = equipmentCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/equipment", async (req, res) => {
       const newEquipment = req.body;
       console.log(newEquipment);
