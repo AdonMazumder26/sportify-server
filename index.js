4d6135fd8cd68145829934764fbbaa48ffb3f995
adon <ruhulmaani2003@gmail.com>
2025-08-14 18:59:02 +0600

    completed

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sportify-server-bb13lnpeh-adons-projects-af1aa7a5.vercel.app",
+      "https://sportify-client-20714.web.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 // VE4mpmhoXJnl2EJb
-const uri =
-  "mongodb+srv://adon:VE4mpmhoXJnl2EJb@cluster0.r854q02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r854q02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// "mongodb+srv://adon:VE4mpmhoXJnl2EJb@cluster0.r854q02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database = client.db("Cluster0");
     const equipmentCollection = database.collection("equipment");
 
@@ -86,7 +97,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
